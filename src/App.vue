1e3eb407b0d3d6d149ffd690b33c2e2cab6d2886
tutu524606 <524682015@qq.com>
2019-08-30 13:40:29 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style lang="less">
+// 引入重置样式和公用基础样式
+@import "./assets/css/reset.css";
+@import "./assets/css/base.less";
+#app {
+  font-family: "Microsoft Yahei", "Avenir", Helvetica, Arial, sans-serif;
+  // font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 0.16rem;
+  // background-color: #fff;
+  // text-align: center;
+  // color: #2c3e50;
+  // margin-top: 60px;
+}
+.loading {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 0.24rem;
+  span {
+    margin-left: 2%;
+    color: #666;
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,41 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+// 引入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)
+
+import { Icon } from 'vant';
+Vue.use(Icon);
+
+// 引入懒加载lazyload
+import VueLazyLoad from 'vue-lazyload'
+Vue.use(VueLazyLoad, {
+    preLoad: 1.3,
+    error: require('./assets/images/loading.png'),
+    loading: require('./assets/images/loading.png'),
+    attempt: 1,
+    // the default is ['scroll', 'wheel', 'mousewheel', 'resize', 'animationend', 'transitionend']
+    listenEvents: ['scroll']
+});
+
+// 桌面端引入依赖
+// import '@vant/touch-emulator';
+
+// 引入资源请求模块
+import http from './assets/js/http'
+Vue.use(http);
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
+})
