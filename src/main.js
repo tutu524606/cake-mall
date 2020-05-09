30a4b0152f130832de287b06651a71eab387df03
tutu524606 <524682015@qq.com>
2020-05-09 11:11:16 +0800

    5-9

--- src/main.js
+++ src/main.js
@@ -5,32 +5,34 @@ import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
-    // 引入vuex
+// 引入vuex
 import store from './store/index'
 
-// 引入vant
+// 引入vant123
 import Vant from 'vant'
 import 'vant/lib/index.css'
 Vue.use(Vant)
 
-import { Icon } from 'vant';
+import {
+  Icon
+} from 'vant';
 Vue.use(Icon);
 
 // 引入elementUI的loading
 import ElementUI from 'element-ui'
 Vue.use(ElementUI, {
-    size: 'small',
-    zIndex: 3000
+  size: 'small',
+  zIndex: 3000
 });
 // 引入懒加载lazyload
 import VueLazyLoad from 'vue-lazyload'
 Vue.use(VueLazyLoad, {
-    preLoad: 1.3,
-    error: require('./assets/images/loading.png'),
-    loading: require('./assets/images/loading.png'),
-    attempt: 1,
-    // the default is ['scroll', 'wheel', 'mousewheel', 'resize', 'animationend', 'transitionend']
-    listenEvents: ['scroll']
+  preLoad: 1.3,
+  error: require('./assets/images/loading.png'),
+  loading: require('./assets/images/loading.png'),
+  attempt: 1,
+  // the default is ['scroll', 'wheel', 'mousewheel', 'resize', 'animationend', 'transitionend']
+  listenEvents: ['scroll']
 });
 
 // 桌面端引入依赖
@@ -44,9 +46,11 @@ Vue.use(http);
 import './assets/css/font/iconfont.css'
 /* eslint-disable no-new */
 new Vue({
-    el: '#app',
-    router,
-    store,
-    components: { App },
-    template: '<App/>'
+  el: '#app',
+  router,
+  store,
+  components: {
+    App
+  },
+  template: '<App/>'
 })
